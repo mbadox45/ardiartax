d9c77dbc012dacbc74aa9c41c5c41d17ea408751
Rio Teguh A <mbadox45@gmail.com>
2026-04-20 07:37:14 +0700

    update PEB

--- src/app/admin/peb/list-peb-tab.tsx
+++ src/app/admin/peb/list-peb-tab.tsx
@@ -10,11 +10,30 @@ import { toast } from "sonner"
 interface ListPebTabProps {
     data: PebData[]
     isLoading: boolean
+    selectedIds: (string | number)[]
+    setSelectedIds: (ids: (string | number)[]) => void
 }
 
-export default function ListPebTab({ data, isLoading }: ListPebTabProps) {
-
+export default function ListPebTab({ data, isLoading, selectedIds, setSelectedIds }: ListPebTabProps) {
+    const [rowSelection, setRowSelection] = useState({})
     const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        const selectedRows = Object.keys(rowSelection)
+        const selectedRealIds = selectedRows.map((index) => {
+            const rowIndex = parseInt(index)
+            return data[rowIndex]?.id // Ambil ID dari data asli berdasarkan index
+        }).filter(Boolean) // Pastikan ID tidak undefined
+
+        setSelectedIds(selectedRealIds)
+    }, [rowSelection, data, setSelectedIds])
+
+    // Reset seleksi jika data di-refresh atau dihapus
+    useEffect(() => {
+        if (selectedIds.length === 0) {
+            setRowSelection({}) // Reset seleksi
+        }
+    }, [selectedIds.length])
     
     if (isLoading) {
         return (
@@ -27,7 +46,7 @@ export default function ListPebTab({ data, isLoading }: ListPebTabProps) {
 
     return (
         <div className="py-4">
-            <UniversalDataTable columns={columns}  data={data} />
+            <UniversalDataTable columns={columns}  data={data}  />
         </div>
     )
 }
