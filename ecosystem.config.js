b2eae92284dec1bf75722fd6674cd9c1f37d1aca
mbadox45 <mbadox45@gmail.com>
2026-06-11 17:18:51 +0700

    feat: update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  apps: [{
-    name: 'KantinFENew-Dev-3033',
-    script: 'node_modules/next/dist/bin/next', // Mengarah langsung ke core next
-    args: 'dev -p 3033',                       // Menentukan port 3033 untuk mode dev
-    watch: true,                               // Memantau perubahan file
+  apps : [{
+    name: 'Ardiartax (3033/tcp)',
+    script: "npm",
+    args: "run dev",
+    watch: true,
     cron_restart: '0 */8 * * *',
     env: {
        NODE_ENV: "development"
+    },
+    env_production: {
+       NODE_ENV: "production"
     }
   }]
 };
